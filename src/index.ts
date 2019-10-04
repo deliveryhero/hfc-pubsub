08dc1137ea8aba155e37077377cc792cf796ef41
Anthony Martin <anthony.martin@honestfoodcompany.de>
2019-10-04 12:02:45 +0200

    exporting SubscriptionService

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Topic, { Payload } from "./topic";
 import Subscription from "./subscription";
+import SubscriptionService from "./subscription.service";
 
-export { Topic, Payload, Subscription };
+export { Topic, Payload, Subscription, SubscriptionService };
