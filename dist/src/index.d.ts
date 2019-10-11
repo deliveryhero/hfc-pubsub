09f678ce1fb5c73e2cdf429cb3e85d89c8466e01
Anthony Martin <anthony.martin@honestfoodcompany.de>
2019-10-11 18:34:09 +0200

    updated package build

--- dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import Topic, { Payload } from "./topic";
-import Subscription from "./subscription";
-import SubscriptionService from "./subscription.service";
-import { Message } from "@google-cloud/pubsub";
-export { Topic, Payload, Subscription, SubscriptionService, Message };
