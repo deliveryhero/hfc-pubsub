55a527bbf7512ce77e033aa295abfd8809ef4d29
Anthony Martin <anthony.martin@honestfoodcompany.de>
2020-07-06 01:36:19 +0200

    simplifies class subscriber

--- __tests__/pubsub/subscriptions/example.v2.subscription.js
+++ __tests__/pubsub/subscriptions/example.v2.subscription.js
@@ -1,16 +1,15 @@
+/* eslint-disable @typescript-eslint/camelcase */
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
 const pubsub_1 = require('../../../dist');
 
 class TestSubscriptionV2 extends pubsub_1.Subscriber {
-  constructor() {
-    super();
-    this.metadata = {
-      topicName: 'test-topic',
-      subscriptionName: 'test-topic.v2-subscription',
-      description: 'Just a test subscription',
-    };
-  }
+  metadata = {
+    topicName: 'test-topic',
+    subscriptionName: 'test-topic.v2-subscription',
+    description: 'Just a test subscription',
+  };
+
   async handleMessage(message) {
     const payload = JSON.parse(message.data.toString());
     message.ack();
