360b88bc2d174727bc209afe05d0d57ba28d0936
Anthony Martin <anthony.martin@honestfoodcompany.de>
2020-07-06 20:00:11 +0200

    updates packages

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.PubSubService = exports.Message = exports.SubscriptionService = exports.Subscriber = exports.Topic = void 0;
 require('dotenv').config({ path: require('find-config')('.env') });
 const topic_1 = __importDefault(require("./topic"));
 exports.Topic = topic_1.default;
