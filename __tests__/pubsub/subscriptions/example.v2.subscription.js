dc42817a9b3f6631a441a2b337a35758ccf35565
Anthony Martin <anthony.martin@honestfoodcompany.de>
2020-07-06 13:54:01 +0200

    fixes tests

--- __tests__/pubsub/subscriptions/example.v2.subscription.js
+++ __tests__/pubsub/subscriptions/example.v2.subscription.js
@@ -1,15 +1,15 @@
-/* eslint-disable @typescript-eslint/camelcase */
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
-const pubsub_1 = require('../../../dist');
-
-class TestSubscriptionV2 extends pubsub_1.Subscriber {
-  metadata = {
-    topicName: 'test-topic',
-    subscriptionName: 'test-topic.v2-subscription',
-    description: 'Just a test subscription',
-  };
-
+const index_1 = require('../../../dist/index');
+class TestSubscriptionV2 extends index_1.Subscriber {
+  constructor() {
+    super(...arguments);
+    this.metadata = {
+      topicName: 'test-topic',
+      subscriptionName: 'test-topic.v2-subscription',
+      description: 'Just a test subscription',
+    };
+  }
   async handleMessage(message) {
     const payload = JSON.parse(message.data.toString());
     message.ack();
