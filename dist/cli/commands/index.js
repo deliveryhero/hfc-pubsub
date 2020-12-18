83248f3664446adf9acacad484528ec8bc460d52
Samuel Clark <samrocksc@gmail.com>
2020-12-18 13:28:31 +0100

    Remove dist

--- dist/cli/commands/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const list_1 = __importDefault(require("./list"));
-const start_1 = __importDefault(require("./start"));
-const subscriptions_1 = __importDefault(require("./subscriptions"));
-exports.default = { start: start_1.default, list: list_1.default, subscriptions: subscriptions_1.default };
--- dist/service/subscriberLoader.d.ts
+++
@@ -1,8 +0,0 @@
-import { Subscribers } from '../subscriber';
-import { SubscriptionServiceFile } from './resourceResolver';
-import { SubscriberOptions } from 'subscriber/subscriberV2';
-export default class SubscriberLoader {
-    loadSubscribersFromDirectory(dir: string, defaultOptions: SubscriberOptions): Subscribers;
-    loadSubscribersFromService(subscriptionService: SubscriptionServiceFile, defaultOptions: SubscriberOptions): Subscribers;
-    private loadSubscriber;
-}
