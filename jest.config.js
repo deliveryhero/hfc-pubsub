da1f3246248309295903dbd31f71ecfe06bcdcbf
Rohit Gohri <rohit.gohri@honestfoodcompany.de>
2021-05-17 22:33:51 +0530

    👷 add github workflow and update scripts

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: TEST_REGEX,
+  coverageReporters: ['html', 'text-summary', 'lcov'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
