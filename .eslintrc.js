54a4af2d9b0937f55c61c6bb87f1e6f950429f1f
Rohit Gohri <rohit.gohri@honestfoodcompany.de>
2021-06-17 10:03:56 +0530

    🔧 enable no-unresolved rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,13 +13,21 @@ module.exports = {
   rules: {
     'prettier/prettier': 'error',
     'import/no-cycle': 'error',
+    'import/no-unresolved': 'error',
     '@typescript-eslint/no-var-requires': 'warn',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
   },
   overrides: [
     {
       // For tests
-      files: ['*.test.{ts,js}', './**/__tests__/*.*'],
+      files: ['*.test.{ts,js}', './**/__tests__/**/*.*'],
       env: {
         jest: true,
       },
@@ -28,6 +36,13 @@ module.exports = {
         // These are off since we require dynamically after mocks are set
         '@typescript-eslint/no-var-requires': 'off',
         'global-require': 'off',
+      },
+    },
+    {
+      // For examples
+      files: ['./examples/**/*.*'],
+      rules: {
+        'import/no-unresolved': 'warn',
       },
     },
   ],
