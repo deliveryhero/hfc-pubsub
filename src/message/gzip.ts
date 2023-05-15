a220ff7cb34f729a33fd8ecc05b268fe1e9a6d73
Alexey Kalmakov <st00nsa@gmail.com>
2023-05-15 10:21:14 +0200

    Update src/message/gzip.ts
    Co-authored-by: Rohit Gohri <rohit.gohri@honestfoodcompany.de>

--- src/message/gzip.ts
+++ src/message/gzip.ts
@@ -1,4 +1,11 @@
-// @see https://en.wikipedia.org/wiki/Gzip#:~:text=%22gzip%22%20is%20often%20also%20used,and%20the%20operating%20system%20ID.
+/**
+ * gzip files contain a 10-byte header, containing a:
+ * - magic number (1f 8b)
+ * - the compression method (08 for DEFLATE)
+ * - 1-byte of header flags, a 4-byte timestamp, compression flags and the operating system ID.
+ *
+ * @see https://en.wikipedia.org/wiki/Gzip#File_format
+ */
 export const isGzipCompressed = (buf: Buffer): boolean => {
   return buf[0] === 0x1f && buf[1] === 0x8b && buf[2] === 0x08;
 };
